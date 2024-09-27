ffd87d7e110c4da665d7e1adcce0704cef2f22d8
hiylx <hiylx>
2024-09-27 08:01:51 +0530

    fix some stuff up

--- backup.sh
+++ backup.sh
@@ -119,7 +119,7 @@ fi
 
 printg " [*] Downloading com.apple.commcenter.device_specific_nobackup.plist "
 echo ./sshpass -p "$termpw" ssh -o StrictHostKeyChecking=no mobile@$devip sudo su root -c 'cp /private/var/wireless/Library/Preferences/com.apple.commcenter.device_specific_nobackup.plist /private/var/containers/Data/System/'
-./sshpass -p "$termpw" ssh -o StrictHostKeyChecking=no mobile@$devip sudo su root -c 'cp /private/var/wireless/Library/Preferences/com.apple.commcenter.device_specific_nobackup.plist /private/var/containers/Data/System/'
+./sshpass -p "$termpw" ssh -o StrictHostKeyChecking=no mobile@$devip sudo su root -c echo "$termpw" | sudo -S cp /private/var/wireless/Library/Preferences/com.apple.commcenter.device_specific_nobackup.plist /private/var/containers/Data/System/
 ./sshpass -p "$termpw" sftp -oPort=2222 StrictHostKeyChecking=no mobile@$devip:/private/var/containers/Data/System/com.apple.commcenter.device_specific_nobackup.plist "$script_path/Activation"
 
 if [ -e "$script_path/Activation/com.apple.commcenter.device_specific_nobackup.plist" ]; then
