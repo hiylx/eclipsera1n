7178b2e5e551613b9dfb1ee2ba65cfc3e06ff51d
hiylx <hiylx>
2024-09-28 15:55:18 +0530

    fixing

--- activate.sh
+++ activate.sh
@@ -196,11 +196,8 @@ printg "[*] Rebooting"
 printg "[*] Script done, your device should be reboot and you can go through setup like normal"
 printg "[*] Your device will work like normal but for you can only use trollstore for side loading. If you want to install it you can check out the trollstore guide"
 
-if [ -f $script_path/knownhosts/known_hosts ]
 printg " [*] Restoring known_hosts file"
 cd $script_path/knownhosts && cp "$script_path/knownhosts/known_hosts" "${HOME}/.ssh/known_hosts"
-fi
-
 
 
 printg "[*] All done! Enjoy iOS 15."
